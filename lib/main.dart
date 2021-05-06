2cd8518b4586da1c76921d64f647fe0b40605a93
Helton Ricardo <50843386+heltonr13@users.noreply.github.com>
2021-05-06 12:11:01 -0300

    tela de nova transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,105 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Transferências"),
-        ),
-        body: ListaTransferencias(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Nova transferência"),
+        ),
+        body: Column(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.account_balance),
+                  labelText: "Banco",
+                  hintText: "000"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.business),
+                  labelText: "Agência",
+                  hintText: "0000"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.account_box_rounded),
+                  labelText: "Conta corrente",
+                  hintText: "00000"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: "Valor",
+                  hintText: "0,00",
+                )),
+          ),
+          ElevatedButton(
+            child: Text("Efetuar transferência"),
+          ),
+        ]));
+  }
+}
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return (Column(
-      children: [
-        ItemTransferencia(Transferencia(100.9, "123-6")),
-        ItemTransferencia(Transferencia(200.8, "456-5")),
-        ItemTransferencia(Transferencia(300.7, "789-4")),
-      ],
-    ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transferências"),
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(100.9, "123-6")),
+          ItemTransferencia(Transferencia(200.8, "456-5")),
+          ItemTransferencia(Transferencia(300.7, "789-4")),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
